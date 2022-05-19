3727eaeb90b32cc7f549768641acba25e0336cc8
Kongi <tielinli@yahoo.com>
2022-05-18 20:55:14 -0500

    Constan

---
+++ constants.js
@@ -0,0 +1,11 @@
+const indexPageBackground = 'f0ebf6eb-3a43-4995-aa2d-fcdcf0c5be7c';
+
+const termLink = 'b451abc2-fdd1-4257-aa49-d335f7b1c851';
+const apiDocLink = 'b451abc2-fdd1-4257-aa49-d335f7b1c851';
+
+const loginPageBanner = 'b1a9a79e-86d6-4c30-b3a9-a6fed258bd5f';
+
+const testFileLink = 'b7bbee65-7a48-417c-850a-c32aea57debd';
+
+const testServiceKey = 'd30e8da5-1d4f-4d2c-b433-5b604f04e9e9';
+const testStorageId = 1;
